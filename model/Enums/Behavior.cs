a9904912b984f07e3d78997b4dac41d953ad40de
Dev-Chiho <53746796+Dev-Chiho@users.noreply.github.com>
2019-08-05 18:03:21 +0900

    Create Behavior.cs

---
+++ model/Enums/Behavior.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+
+namespace CustomBot.Enums
+{
+    /// <summary>
+    /// 행위에 대한 열거형 타입
+    /// </summary>
+    public enum Behavior
+    {
+        [Description("Telegram")]
+        Telegram
+    }
+}
