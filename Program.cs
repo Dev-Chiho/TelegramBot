a6023cdcca01fdbb19e582ba1e9a0630e0c4cd3c
Dev-Chiho <53746796+Dev-Chiho@users.noreply.github.com>
2019-08-05 18:09:15 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,22 +12,5 @@ namespace CustomBot
             var generate = Generate.Instance();
             generate.Excute(Behavior.Telegram);
         }
-
-        //static void Main(string[] args)
-        //{
-        //    // 봇 접근 함수 호출
-        //    APIAsync();
-        //    System.Console.ReadLine();
-        //}
-
-        //// 비동기 봇 접근
-        //static async void APIAsync()
-        //{
-        //    var Bot = new TelegramBotClient(GlobalData.Variable._telegramToken);
-        //    var me = await Bot.GetMeAsync();
-
-        //    //올바르고 접속이 되면 봇 이름이 출력된다.
-        //    System.Console.WriteLine($"Hello my name is {me.FirstName}");
-        //}
     }
 }
